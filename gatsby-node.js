37e5c31b9be7e4847eddba813d957fbb166fd897
Jeremy Barnes <jeremy@wovenmagazine.com>
2019-11-20 06:00:08 -0800

    my first blog commit.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,55 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const path = require("path")
+
 // You can delete this file if you're not using it
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(
+    `
+      {
+        allStrapiArticle {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const posts = result.data.allStrapiArticle.edges
+
+  posts.forEach(({ node }) => {
+    createPage({
+      path: `/${node.id}`,
+      component: path.resolve(`src/templates/article.js`),
+      context: { id: node.id },
+    })
+  })
+
+  const getAuthors = await graphql(`
+    {
+      allStrapiUser {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `)
+
+  getAuthors.data.allStrapiUser.edges.forEach(({ node }) => {
+    createPage({
+      path: `/authors/${node.id}`,
+      component: path.resolve(`src/templates/author.js`),
+      context: {
+        id: node.id,
+      },
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,57 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Img from "gatsby-image"
+import ReactMarkdown from "react-markdown"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  var {
+    allStrapiArticle: { edges },
+  } = data
+  console.log(edges)
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <ul>
+        {edges.map(({ node }, index) => {
+          return (
+            <li key={node.id} article={node}>
+              <h2>
+                <Link to={`/${node.id}`}>{node.title}</Link>
+              </h2>
+              <Img fluid={node.image.childImageSharp.fluid}></Img>
+              {/* <ReactMarkdown source={node.content} /> */}
+            </li>
+          )
+        })}
+      </ul>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          title
+          content
+          image {
+            childImageSharp {
+              fluid(maxWidth: 960) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
